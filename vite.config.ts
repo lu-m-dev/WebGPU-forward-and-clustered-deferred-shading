dc24ee1acf28bc07322cdd777fcc65c07b068e66
M320322 <menlu2019@gmail.com>
2025-11-11 19:09:03 -0500

    configure vite base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 
+declare const process: any
+
 export default defineConfig({
     build: {
         target: 'esnext'
     },
-    base: process.env.GITHUB_ACTIONS_BASE || undefined
+    base: process.env.GITHUB_ACTIONS_BASE || './'
 })
